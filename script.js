897e2df25f738a8b448a2e138f1b6d7e5908c4b1
Hector Valenzuela <francisco1495@gmail.com>
2023-03-19 17:26:47 -0700

    Agregamos interactividad a la descripcion de cada tecnologia

--- script.js
+++ script.js
@@ -2,17 +2,27 @@ var explorebtn = document.querySelector('.explorebtn')
 var navbar = document.querySelector('.navigation-bar')
 var navbarElement = document.querySelectorAll('.navbar-element')
 var techDescription = document.querySelectorAll('.tech-description')
+
+/*Hobbies Pictures */
+var hobbyImage = document.querySelectorAll('.hobby-image')
+var hobbyBigPics = document.querySelectorAll('.hobby-big-pics')
+
+/*Technologies */
 var htmlTech = document.querySelector('.html-tech')
 var cssTech = document.querySelector('.css-tech')
 var jsTech = document.querySelector('.js-tech')
 var gitTech = document.querySelector('.git-tech')
 var githubTech = document.querySelector('.github-tech')
+
+/*Technologies Description */
 var htmlDescription = document.querySelector('#html-description')
 var cssDescription = document.querySelector('#css-description')
 var jsDescription = document.querySelector('#js-description')
 var gitDescription = document.querySelector('#git-description')
 var gitHubDescription = document.querySelector('#gitHub-description')
 var techdescCloseBtn = document.querySelectorAll('.closebtn')
+ 
+/*Darken Website */
 var darken = document.querySelector('.darken')
 var darkenToggle = document.querySelector('.darken-inactive')
 
@@ -22,7 +32,7 @@ cssTech.addEventListener('click', showCSSdescription)
 jsTech.addEventListener('click', showJsdescription)
 gitTech.addEventListener('click', showGitdescription)
 githubTech.addEventListener('click', showGitHubdescription)
-
+darken.addEventListener('click', closeTechDescription)
 
 function showNavBarList(){
     navbar.classList.toggle('inactive-navbar')
@@ -32,6 +42,20 @@ navbarElement.forEach(navbarElement => {
     navbarElement.addEventListener('click', function hideNavbarList() {
         navbar.classList.add('inactive-navbar')    
     })
+});
+
+
+hobbyImage.forEach(hobbyImage => {
+    hobbyImage.addEventListener('click', showHobbyImages)
+    function showHobbyImages(e){
+        for(var i=1; i = hobbyBigPics.length - 1; i++){
+            if (e.target === hobbyBigPics[0]){
+                hobbyBigPics[0].classList.remove('hobbies-inactive')
+            }else if(e.target === hobbyBigPics[1]){
+                hobbyBigPics[1].classList.remove('hobbies-inactive')
+            }
+        }
+    }
 });
 
 function showhtmldescription(){
@@ -48,6 +72,7 @@ function showCSSdescription(){
     jsDescription.classList.add('inactive-tech-description')
     gitDescription.classList.add('inactive-tech-description')
     gitHubDescription.classList.add('inactive-tech-description')
+    darken.classList.remove('darken-inactive')
 }
 function showJsdescription(){
     jsDescription.classList.toggle('inactive-tech-description')
@@ -55,6 +80,7 @@ function showJsdescription(){
     htmlDescription.classList.add('inactive-tech-description')
     gitDescription.classList.add('inactive-tech-description')
     gitHubDescription.classList.add('inactive-tech-description')
+    darken.classList.remove('darken-inactive')
 }
 function showGitdescription(){
     gitDescription.classList.toggle('inactive-tech-description')
@@ -62,6 +88,7 @@ function showGitdescription(){
     cssDescription.classList.add('inactive-tech-description')
     htmlDescription.classList.add('inactive-tech-description')
     gitHubDescription.classList.add('inactive-tech-description')
+    darken.classList.remove('darken-inactive')
 }
 function showGitHubdescription(){
     gitHubDescription.classList.toggle('inactive-tech-description')
@@ -69,10 +96,12 @@ function showGitHubdescription(){
     jsDescription.classList.add('inactive-tech-description')
     cssDescription.classList.add('inactive-tech-description')
     htmlDescription.classList.add('inactive-tech-description')
+    darken.classList.remove('darken-inactive')
 
 }
 
-/*Closing technologies description by using the CloseBtn */ 
+
+/*Closing technologies description and darken by using the CloseBtn */ 
 techdescCloseBtn.forEach(techdescCloseBtn =>{
     techdescCloseBtn.addEventListener('click', hideTechDescription)
     function hideTechDescription(e){
@@ -80,8 +109,21 @@ techdescCloseBtn.forEach(techdescCloseBtn =>{
             if (e.target !== techDescription[i]){
                 techDescription[i].classList.add('inactive-tech-description')
             }
+        
         }   
     }
-})
+    techdescCloseBtn.addEventListener('click', hideDarken)
+    function hideDarken (){
+        darken.classList.add('darken-inactive')
+    }
+    })
 
 /*Closing Technologies description by Clicking outside of the description*/
+function closeTechDescription(e){
+    for(var i=0; i < techDescription.length; i++){
+        if(e.target !== techDescription[i]){
+            techDescription[i].classList.add('inactive-tech-description')
+        }
+    }
+    darken.classList.add('darken-inactive')
+}
